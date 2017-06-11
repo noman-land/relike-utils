fd5fa4dce542ad814c8d54e05906d9ad45c569f7
Noman <noman@noman.land>
2017-06-10 22:27:53 -0400

    Rebuild for 0.1.2

--- build/index.js
+++ build/index.js
@@ -1,4 +1,14 @@
-/******/ (function(modules) { // webpackBootstrap
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["relike-utils"] = factory();
+	else
+		root["relike-utils"] = factory();
+})(this, function() {
+return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
 /******/
@@ -28811,3 +28821,4 @@ module.exports = XMLHttpRequest;
 
 /***/ })
 /******/ ]);
+});
