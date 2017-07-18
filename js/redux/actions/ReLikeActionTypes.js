44fd955b224f883fcf207dba3f040906d62001b7
Noman <noman@noman.land>
2017-07-18 17:18:44 -0400

    Update NEW_LIKE action type name

--- js/redux/actions/ReLikeActionTypes.js
+++ js/redux/actions/ReLikeActionTypes.js
@@ -30,7 +30,7 @@ export default {
   LIKE_START: prefix('LIKE_START'),
   LIKE_SUCCESS: prefix('LIKE_SUCCESS'),
 
-  NEW_LIKE_EVENT: prefix('NEW_LIKE'),
+  NEW_LIKE_EVENT: prefix('NEW_LIKE_EVENT'),
 
   UNDISLIKE: prefix('UNDISLIKE'),
   UNDISLIKE_ERROR: prefix('UNDISLIKE_ERROR'),
--- js/redux/actions/likeEventActions.js
+++ js/redux/actions/likeEventActions.js
@@ -3,6 +3,6 @@ import { createAction } from 'redux-actions';
 import actionTypes from './ReLikeActionTypes';
 
 export const newLikeEvent = createAction(
-  actionTypes.NEW_LIKE,
+  actionTypes.NEW_LIKE_EVENT,
   entityId => ({ entityId }),
 );
