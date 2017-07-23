cac84f6be9d42ea9b20f33bc7b2a5b47e43c9d6a
Noman <noman@noman.land>
2017-07-23 02:46:07 +0200

    Remove unused file

--- js/redux/actions/likeEventActions.js
+++
@@ -1,8 +0,0 @@
-import { createAction } from 'redux-actions';
-
-import actionTypes from './ReLikeActionTypes';
-
-export const newLikeEvent = createAction(
-  actionTypes.NEW_LIKE_EVENT,
-  entityId => ({ entityId }),
-);
